f83de3f6862ef406a85e4d36de282f7b582a2719
Ross Hochwert <rhochwert@gmail.com>
2014-09-13 11:10:07 -0500

    abnything

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ TradeWhistle::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'index#login'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
