05da90f9910eefca9c2ec6c840ebcd88a7e6e7a8
Ross Hochwert <rhochwert@gmail.com>
2014-09-13 15:27:45 -0500

    trying something new

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,18 +1,31 @@
 class Team < ActiveRecord::Base
 	has_many :players
 
-	def self.import(data)
-		#loop through each team in data, create a team object for them
-		for team in data
 
-			puts team
-			puts team["name"]
-			@newTeam = Team.create(:name => team["name"])
-
-			#loop through all the players in a team (assuming they get returned and add them)
-			for player in team
-				@player = @newTeam.players.create(:name => player["name"])
-			end
+	def self.impot(data)
+		data.each do |key, value|
+		  alert key
+		  value.each do |k,v|
+		    alert k
+		    alert v
+		  end
 		end
+
 	end
+
+
+	# def self.import(data)
+	# 	#loop through each team in data, create a team object for them
+	# 	for team in data
+
+	# 		puts team
+	# 		puts team["name"]
+	# 		@newTeam = Team.create(:name => team["name"])
+
+	# 		#loop through all the players in a team (assuming they get returned and add them)
+	# 		for player in team
+	# 			@player = @newTeam.players.create(:name => player["name"])
+	# 		end
+	# 	end
+	# end
 end
