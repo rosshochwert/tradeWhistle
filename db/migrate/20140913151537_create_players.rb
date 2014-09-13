91f6f15c8b217a99544de2643ccdcb937383df4a
Ross Hochwert <rhochwert@gmail.com>
2014-09-13 10:33:20 -0500

    added bootstrap rails

--- db/migrate/20140913151537_create_players.rb
+++ db/migrate/20140913151537_create_players.rb
@@ -3,7 +3,7 @@ class CreatePlayers < ActiveRecord::Migration
     create_table :players do |t|
       t.string :first_name
       t.string :last_name
-      t.int :number
+      t.integer :number
       t.references :team, index: true
 
       t.timestamps
