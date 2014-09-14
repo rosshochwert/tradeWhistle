d36df72c80d9118b8bfd34141b0af955caec7268
azpang <azpang124@gmail.com>
2014-09-13 22:50:09 -0500

    Working colors and league view

--- app/views/team_view.js
+++ app/views/team_view.js
@@ -2,54 +2,192 @@
 
 
 var league = {};
-var tempPlayer = {
-    "position": "RB", 
-    "projected": 15,
-    "prk": 5,
-    "owned": .123
+
+var qbs =["Matt Ryan","Matthew Stafford","Andrew Luck","Peyton Manning","Carson Palmer","Jake Locker","Derek Anderson","Andy Dalton","Jay Cutler","Colin Kaepernick","Russell Wilson","Ben Roethlisberger","Chad Henne","Drew Brees","Joe Flacco","EJ Manuel","Matt Cassel","Nick Foles","Derek Carr","Josh McCown","Ryan Fitzpatrick","Brian Hoyer","Ryan Tannehill","Philip Rivers","Geno Smith","Tony Romo","Alex Smith","Aaron Rodgers","Robert Griffin","Eli Manning","Austin Davis","Shaun Hill","Jon Kitna","Matt Hasselbeck","Michael Vick",];
+var rbs =["Marshawn Lynch","Knowshon Moreno","Mark Ingram","DeMarco Murray","Matt Forte","Chris Ivory","Rashad Jennings","Isaiah Crowell","Darren Sproles","Chris Johnson","Justin Forsett","Montee Ball","C.J. Spiller","Ryan Mathews","Shane Vereen","LeSean McCoy","Antone Smith","Joique Bell","Carlos Hyde","Lamar Miller","Giovani Bernard","Terrance West","Arian Foster","Jacquizz Rodgers","Alfred Morris","Adrian Peterson*","Ahmad Bradshaw","Pierre Thomas","Stepfan Taylor","Khiry Robinson","DeAngelo Williams","Fred Jackson","Shonn Greene","Derrick Coleman","Frank Gore","John Kuhn","Darrel Young","Anthony Dixon","LeGarrette Blount","Steven Jackson","Reggie Bush","Jorvorskie Lane","Toby Gerhart","Roy Helu","Trent Richardson","Bobby Rainey","Andre Ellington","Benjamin Cunningham","Ben Tate*","James Starks","Eddie Lacy","Zac Stacy","Leon Washington","Jonathan Stewart","Dexter McCluster","Maurice Jones-Drew","Jamaal Charles","Jonathan Dwyer","Stevan Ridley","Robert Turbin","Lance Dunbar","C.J. Anderson","Bishop Sankey","Devonta Freeman","Darren McFadden","Mike Tolbert","Matt Asiata","Jeremy Hill","Willis McGahee","Greg Jones","Vonta Leach","Ronnie Brown","Brandon Jacobs","Michael Robinson","Montell Owens","Brian Leonard","Michael Bush","Le'Ron McClain","Jason Snelling","Jackie Battle","Felix Jones","Rashard Mendenhall","Ray Rice","Tashard Choice","Jerome Felton","Jalen Parmele","Spencer Larsen","Lex Hilliard","Peyton Hillis","Jed Collins","Marcel Reece","BenJarvus Green-Ellis","Darius Reynaud","Kahlil Bell","Donald Brown","Andre Brown","Tony Fiammetta","Mike Goodson"];
+var wrs =["Allen Hurns","A.J. Green","Cordarrelle Patterson","Steve Smith Sr.","Antonio Brown","Jeremy Maclin","Kelvin Benjamin","DeAndre Hopkins","Brandin Cooks","Roddy White","Mike Wallace","Malcom Floyd","Greg Jennings","Brandon Marshall","Julian Edelman","Randall Cobb","Michael Floyd","Terrance Williams","Kendall Wright","Rod Streater","Reggie Wayne","Andre Johnson","Anquan Boldin","Devin Hester","Marques Colston","James Jones","Percy Harvin","Hakeem Nicks","Golden Tate","Julio Jones","Ricardo Lockette","Brian Quick","Travis Benjamin","Markus Wheaton","Donnie Avery","Jordy Nelson","Emmanuel Sanders","Andrew Hawkins","Chris Owusu","John Brown","Pierre Garcon","Eric Decker","Alshon Jeffery","Robert Woods","DeSean Jackson","Harry Douglas","Justin Hunter","Marqise Lee","Nate Washington","Dez Bryant","Torrey Smith","Dwayne Harris","Mohamed Sanu","Demaryius Thomas","T.Y. Hilton","Cole Beasley","Jerricho Cotchery","Vincent Jackson","Stevie Johnson","Dexter McCluster","Mike Williams","Jeremy Kerley","Kamar Aiken","Chris Givens","Mike Brown","Tavon Austin","Keenan Allen","Justin Brown","Kenbrell Thompkins","Sammy Watkins","Mike Evans","Jordan Matthews","Larry Fitzgerald","Santonio Holmes","Miles Austin","Ted Ginn Jr.","Eddie Royal","Michael Crabtree","Brian Hartline","Riley Cooper","Andre Roberts","Victor Cruz","Jerrel Jernigan","Jarius Wright","Frankie Hammond","Jacoby Jones","Andre Caldwell","Danny Amendola","Brandon Tate","David Alan Nelson","Greg Salas","Doug Baldwin","Jeff Maehl","Devin Street","Taylor Gabriel","Brandon Stokley","Plaxico Burress","Santana Moss","Nate Burleson"];
+var tes =["Julius Thomas","Vernon Davis","Greg Olsen","Martellus Bennett","Zach Ertz","Dwayne Allen","Larry Donnell","Rob Gronkowski","Delanie Walker","Antonio Gates","Anthony Fasano","Dennis Pitta","Jimmy Graham","Kyle Rudolph","Levine Toilolo","Niles Paul","Jared Cook","Jeff Cumberland","Zach Miller","Brandon Myers","Jordan Cameron","Travis Kelce","Joseph Fauria","Marcedes Lewis","Owen Daniels","Jim Dray","Tyler Eifert*","Mychal Rivera","Heath Miller","John Carlson","Andrew Quarless","Charles Clay","Coby Fleener","Ladarius Green","Rhett Ellison","Taylor Thompson","Austin Seferian- Jenkins*","Jason Witten","Dante Rosario","Brent Celek","Gary Barnidge","Jermaine Gresham","Lance Kendricks","Rob Housler","Tim Wright","Brian Leonhardt","Tony Gonzalez","Dallas Clark","Visanthe Shiancoe","Kellen Winslow"];
+var dsts =["Chicago Bears","Arizona Cardinals","Green Bay Packers","New York Giants","Detroit Lions","Washington Redskins","Pittsburgh Steelers","Philadelphia Eagles","St. Louis Rams","San Francisco 49ers","Cleveland Browns","Indianapolis Colts","Dallas Cowboys","Oakland Raiders","New England Patriots","Tennessee Titans","Denver Broncos","San Diego Chargers","New York Jets","Kansas City Chiefs","Buffalo Bills","Minnesota Vikings","Miami Dolphins","Atlanta Falcons","New Orleans Saints","Cincinnati Bengals","Seattle Seahawks","Tampa Bay Buccaneers","Jacksonville Jaguars","Carolina Panthers","Baltimore Ravens","Houston Texans"];
+var ks =["Matt Bryant","Mike Nugent","Ryan Succop","Caleb Sturgis","Shaun Suisham","Dan Carpenter","Blair Walsh","Shayne Graham","Cody Parkey","Stephen Gostkowski","Steven Hauschka","Billy Cundiff","Robbie Gould","Nick Folk","Greg Zuerlein","Nate Freese","Graham Gano","Chandler Catanzaro","Brandon McManus","Adam Vinatieri","Randy Bullock","Nick Novak","Dan Bailey","Josh Scobee","Mason Crosby","Phil Dawson","Justin Tucker","Cairo Santos","Sebastian Janikowski","Josh Brown","Patrick Murray","David Akers","Rian Lindell"];
+
+
+function draftpos(round){
+	var pos = "";
+	var poslist;
+	if(round <= 2){
+		pos = "QB";
+		poslist = qbs;
+	} else if (round <= 6){
+		pos = "RB";
+		poslist = rbs;
+	} else if (round <= 11){
+		pos = "WR";
+		poslist = wrs;
+	} else if (round <= 13){
+		pos = "TE";
+		poslist = tes;
+	} else if (round <= 14){
+		pos = "DST";
+		poslist = dsts;
+	} else {
+		pos = "K";
+		poslist = ks;
+	}
+
+	var picknum = Math.floor((Math.random() * poslist.length));
+	var name = poslist[picknum];
+    poslist.splice(picknum,1);
+	var projected = Math.random() * 25;
+	var owned = Math.random();
+
+	var player = {
+		"name": name,
+		"position": pos,
+		"projected": projected,
+		"prk": picknum,
+		"owned": owned
+	}
+
+	return player;
 }
+
+
 var teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9", "Team 10"];
+
 teams.forEach(function(team){
-    league[team] = {};
-    for(i = 0; i <= 14; i++){
-        var player = "Player"+i;
-        league[team][player] = tempPlayer;
+    league[team] = [];
+    console.log(team);
+    for(i = 1; i <= 15; i++){
+        var draftee = draftpos(i);
+        league[team].push(draftee);
+        console.log(draftee);
     }
 })
 
+var projectedScores = [],
+    prk = [],
+    percentOwned =[];
+
+for(var team in league){
+    for(var player in league[team]){ 
+        projectedScores.push(league[team][player]['projected']);
+        prk.push(league[team][player]['prk']);
+        percentOwned.push(league[team][player]['owned']);
+    }
+};
 
 var height = 540;
 
 var team_canvas = d3.select(".cur-team")
-        .style('padding', 10)
         .append("svg")
             .attr("width", "100%")
             .attr('height', 540);
 
-var rectHeight = 36;
+var rectHeight = 36,
+    smallHeight = 15;
+
+var valueScale = d3.scale.quantize()
+    .domain(d3.extent(projectedScores))
+    .range(['#109510','#31ff3a','#f7d129', '#faed00','#de6905','#f70000']);
 
 console.log(d3.entries(league['Team 1']));
 
 var roster = team_canvas.selectAll('g')
-        .data(d3.entries(league['Team 1'])).enter()
+        .data(league['Team 1']).enter()
         .append('g')
             .attr("transform", function(d, i) { return "translate(0," + i * rectHeight + ")"; });
 
 roster.append('rect')
-    .style("stroke", "#000")
-    .style("stroke-width", 2)
     .attr('class', 'roster-spot')
-    .attr('fill', '#b9b7b7')
+    .attr('fill', function(d){
+        return valueScale(d.projected);
+    })
     .attr('width', '100%')
     .attr('height', rectHeight);
 
 roster.append('text')
-    .attr("x", 40)
+    .attr("x", 10)
     .attr("y", rectHeight / 2)
     .attr("dy", ".35em")
     .text(function(d){
-                return d.value.position +" | " + d.key;
+                return d.position +" | " + d.name;
             });
-        
+
+var opponent_canvas = d3.select(".opp-team")
+    .append("svg")
+        .attr("width", "100%")
+        .attr("height", 540);
+
+var opp_roster = opponent_canvas.selectAll('g')
+        .data().enter()
+        .append(
+
+console.log(d3.entries(league));
+
+for (var key in league){
+   var tempCanvas = d3.select(".league")
+        .append('div')
+            .style('margin', '5px')
+            .attr('class', 'col-md-2 small-team')
+            .attr('id', key);
+   
+   tempCanvas.append('span')
+        .text(key);
+    
+    tempCanvas.append('br');
+    
+    var tempSVG = tempCanvas.append('svg')
+                .attr("width", "100%")
+                .attr('height', 250);
+    
+    var tempRoster = tempSVG.selectAll('g')
+        .data(league[key]).enter()
+        .append('g')
+            .attr("transform", function(d, i) { return "translate(0," + i * smallHeight + ")"; });;
+    
+    tempRoster.append('rect')
+        .attr('class', 'small-spot')
+        .attr('fill', function(d){
+            return valueScale(d.projected);
+        })
+        .attr('width', '100%')
+        .attr('height', smallHeight);
+    
+    tempRoster.append('text')
+        .attr('x', 5)
+        .attr('y', smallHeight/2)
+        .attr('dy', '.35em')
+        .text(function(d){
+            return d.position;
+        });
+    
+}
+
+
+$('.metric').click(function(element){
+    if($(this).attr('value') == "projected"){
+        console.log("projected");
+        valueScale.domain(d3.extent(projectedScores));
+    }
+    if($(this).attr('value') == "prk"){
+        console.log("prk");
+        valueScale.domain(d3.extent(prk));
+    }
+    if($(this).attr('value')=="owned"){
+        console.log("owned");
+        valueScale.domain(d3.extent(percentOwned));
+    }
+})
+
+function populateOpponent(){
+    
+}
+
+/*var league_canvas = d3.select(".league")
+        .selectAll('div')
+        .data(d3.entries(league)).enter()
+        .append('div')
+            .style('margin', '5px')
+            .attr('class', 'col-md-2')*/
 
 
