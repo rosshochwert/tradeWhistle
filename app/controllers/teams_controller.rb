f7b2edef467c66402cd7db5f9e90b433e500e8e3
Ross Hochwert <rhochwert@gmail.com>
2014-09-13 22:17:48 -0500

    yahoo is the worst

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -70,7 +70,7 @@ class TeamsController < ApplicationController
 
       @team = Team.find_or_create_by(key: key, name: name)
 
-      @playerHash = getTeamRoster(key)["fantasy_content"]["team"][0][1]["roster"][0]["players"]
+      @playerHash = getTeamRoster(key)["fantasy_content"]["team"][1]["roster"][0]["players"]
       
       @playerHash.each do |keys, values|
         if keys === "count"
